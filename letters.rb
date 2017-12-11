69cf4cd638dd042299d7286cdbfb29ca6cd4f58f
joshua-murphy <josh.murphy11@gmail.com>
2017-12-11 10:35:27 -0700

    clean up

--- letters.rb
+++ letters.rb
@@ -1,20 +1,14 @@
+@num = []
+puts "\nGive me a phrase and I will find the word with the most repeated letters!"
+
 def parse(word)
   letter_counts = []
   word_arr = word.split("").to_a
   word_arr.each { |letter| letter_counts.push(word_arr.count(letter)) }
   @num.push(letter_counts.index(letter_counts.max))
-  check_nums(word_arr)
-  @ind += 1
 end
 
-def check_nums(word_arr)
-  # puts "num #{word_arr[@num[@ind]]}"
-end
-
-@num = []
-@ind = 0
 input = gets.strip
 input_arr = input.split(" ")
 input_arr.each{ |word| parse(word)}
 puts "\n" + input_arr[@num.index(@num.max)].capitalize + " has the highest number of repeated letters.\n "
-# puts @num.max
